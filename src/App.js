4db14515767ebecb9b6d36f7a907a852f6be966b
KodyCoppock <kodycoppock@gmail.com>
2022-05-23 22:18:58 -0400

    Lec 113

--- src/App.js
+++ src/App.js
@@ -1,41 +1,56 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import {connect} from "react-redux";
 import CardList from './CardList';
 import Searchbox from './Searchbox';
 import Scroll from './Scroll';
 import './App.css';
 
-function App() {
-    const [robots, setRobots] = useState([])
-    const [searchfield, setSearchfield] = useState('')
-    const [count, setCount] = useState(0) // for demo purposes
+import {setSearchField} from "./actions";
 
-    useEffect(()=> {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response=> response.json())
-            .then(users => {setRobots(users)});
-        // console.log(count)
-    },[]) // if you add count, only run if count changes.
-
-    const onSearchChange = (event) => {
-        setSearchfield(event.target.value)
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
     }
-
-    const filteredRobots = robots.filter(robot =>{
-        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    })
-
-    return !robots.length ?
-        <h1>Loading</h1> :
-        (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <button onClick={()=>setCount(count+1)}>Click Me!</button>
-                <Searchbox searchChange={onSearchChange}/>
-                <Scroll>
-                    <CardList robots={filteredRobots} />
-                </Scroll>
-            </div>
-        );
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response=> response.json())
+            .then(users => {this.setState({ robots: users})});
+    }
+
+
+    render() {
+        const { robots } = this.state;
+        const filteredRobots = robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(this.props.searchField.toLowerCase());
+        })
+        return !robots.length ?
+            <h1>Loading</h1> :
+            (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <Searchbox searchChange={this.props.onSearchChange}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+                </div>
+            );
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
